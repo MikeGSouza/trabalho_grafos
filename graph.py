d6455b8338dabf9c796389a88b1d3c79d1fcdbde
Mike Gonçalves Souza <97552336+MikeGSouza@users.noreply.github.com>
2024-09-10 11:40:38 -0300

    variação na geração do grafo
    correção na forma de gerar o grafo

--- graph.py
+++ graph.py
@@ -1,24 +1,22 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import json
+import random
 
-data = json.load(open('./dicionario_final.json'))
+data = json.load(open('./dict_relacoes.json'))
 
 G = nx.Graph()
 
 edges = data.get("POLITICS", [])
-print(len(edges))
 
-for edge in edges:
+# Selecionar 100 arestas aleatórias
+random_edges = random.sample(edges, 100)
+
+for edge in random_edges:
     G.add_edge(*edge)
-
-# nx.draw(G, with_labels=True)
-# plt.show()
 
 print("Number of nodes:", G.number_of_nodes())
 print("Number of edges:", G.number_of_edges())
-
-# print("Nodes:", G.nodes())
 
 def calculate_degree_centrality(G):
     # Calculate the degree centrality of the nodes
@@ -31,38 +29,13 @@ def calculate_closeness_centrality(G):
     return centrality
 
 # Example usage
-centrality = calculate_degree_centrality(G)
-print("Degree Centrality:", centrality)
-
-# Step 2: Sort Nodes by Centrality
-sorted_nodes = sorted(centrality.items(), key=lambda x: x[1], reverse=True)
-
-# Step 3: Select Top Nodes (at least 10)
-top_nodes = [node for node, _ in sorted_nodes[:10]]
-
-# Step 4: Create a Subgraph
-subG = G.subgraph(top_nodes)
+degree_centrality = calculate_degree_centrality(G)
+closeness_centrality = calculate_closeness_centrality(G)
+print("Degree Centrality:", degree_centrality)
+print("Closeness Centrality:", closeness_centrality)
 
 # Step 5: Plot the Graph
 plt.figure(figsize=(10, 8))
-nx.draw(subG, with_labels=True, node_size=700, node_color='skyblue', font_size=15, font_weight='bold')
+nx.draw(G, with_labels=True, node_size=600, node_color='skyblue', font_size=5, font_weight='bold')
 plt.title("Top 10 Nodes by Degree Centrality")
-plt.show()
-
-# Assuming G is your graph
-
-# Calculate Node Positions for the entire graph G
-pos = nx.spring_layout(G)
-
-# Find the Center of the Graph
-center_nodes = nx.center(G)
-
-# Plot the Graph
-plt.figure(figsize=(10, 8))
-nx.draw(G, pos, with_labels=True, node_size=700, node_color='skyblue', font_size=15, font_weight='bold')
-
-# Highlight the Center Nodes
-nx.draw_networkx_nodes(G, pos, nodelist=center_nodes, node_size=800, node_color='red')
-
-plt.title("Graph G with Center Highlighted")
 plt.show()
